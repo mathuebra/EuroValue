a125719e44127dc00757a0e9f49076548b35e535
mathuebra <mathuebra@gmail.com>
2025-03-20 00:41:17 +0100

    Tentativa de validação do workflow

--- EuroValue.py
+++ EuroValue.py
@@ -6,34 +6,31 @@ import os
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-# Função para enviar mensagem pelo bot
 def enviar_mensagem(mensagem):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": mensagem}
     requests.post(url, json=payload)
 
-# Função para obter a cotação do Euro
 def obter_cotacao():
     url = "https://economia.awesomeapi.com.br/json/last/EUR-BRL"
     resposta = requests.get(url).json()
     return round(float(resposta["EURBRL"]["bid"]), 2)  # Mantém 2 casas decimais
 
-# Caminho do arquivo de cache
-cache_path = "ultimo_valor.txt"
+# Caminho do arquivo que servirá como cache da última cotação
+cache_path = "VALUE.txt"
 
 # Verifica se o arquivo existe, senão cria com valor padrão
 if not os.path.exists(cache_path):
     with open(cache_path, "w") as f:
         f.write("0.00")
 
-# Lê o último valor do cache
+# Lê o valor da última cotação
 with open(cache_path, "r") as f:
     ultimo_valor = float(f.read().strip())
 
-# Obtém a nova cotação
 cotacao = obter_cotacao()
 
-# Compara e envia mensagem, se necessário
+# Compara e envia mensagem
 if cotacao > ultimo_valor:
     enviar_mensagem(f"🔺 Euro subiu para R$ {cotacao:.2f}")
 elif cotacao < ultimo_valor:
